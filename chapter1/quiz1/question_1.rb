a073ddb90fa2a386b43a40741a43468a01f99ac8
Muzammil Mohamed <muzakkila@gmail.com>
2021-05-17 20:43:47 +0100

    quiz1
    Quiz1

---
+++ chapter1/quiz1/question_1.rb
@@ -0,0 +1,5 @@
+# Write a program that adds 7 to 5, multiplies that by 10, subtracts 2 from all that, divides all that by 4, adds 1,000,000 to all that and `puts`es the result.
+
+# Your answer here.
+
+puts 7 + 5 * 10 - 2 / 4 + 1, 000,000
---
+++ test.rb
@@ -0,0 +1,4 @@
+my_arrauy = [1,2,3,4,5]
+my_arrauy.each do |num|
+  puts num * num
+end
